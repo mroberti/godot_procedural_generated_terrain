a2165edd19afa4c41fc699798fcbedae3cbe2d4a
Mario Roberti <mario.roberti@gmail.com>
2023-07-12 17:37:47 -0700

    Added full spritesheet for fighter
    Lame had to combine spritesheets but got some animations working

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @onready var animations = $AnimationPlayer
 var health = 100
 	
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():	
 	var num = randi_range ( 0,3 )
@@ -13,8 +14,24 @@ func _ready():
 		animations.play("up_right")
 	elif(num==3):
 		animations.play("up_left")
-	
+	add_to_group("monsters")
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _move_randomly():
+	var num = randi_range ( 0,3 )
+	var tile_pos = get_parent().local_to_map(position)
+	print("Move? "+str(tile_pos))
+	if(num==0):
+		tile_pos.x -= 1
+	elif(num==1):
+		tile_pos.x += 1
+	elif(num==2):
+		tile_pos.y -= 1
+	elif(num==3):
+		tile_pos.y += 1
+	position = get_parent().map_to_local(tile_pos)
 
--- scenes/manger/enemy_manager.gd
+++ scenes/manger/enemy_manager.gd
@@ -5,14 +5,14 @@ extends Node
 const SPAWN_RADIUS = 330
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
-	
-	
-func on_timer_timeout():
-	var tile_pos = get_parent().local_to_map(player.position)
-	var enemy = basic_enemy_scene.instantiate() as CharacterBody2D
-	get_parent().add_child(enemy)
-	var rand_x = randi_range ( -5,5 )
-	var rand_y = randi_range ( -5,5 )
-	enemy.position = get_parent().map_to_local (Vector2i(tile_pos.x-rand_x,tile_pos.y-rand_y))
-	print("Adding at "+str(get_parent().local_to_map(enemy.position)))
+	for n in range(10):
+		var tile_pos = get_parent().local_to_map(player.position)
+		var enemy = basic_enemy_scene.instantiate() as CharacterBody2D
+		get_parent().call_deferred("add_child", enemy)
+		var rand_x = randi_range ( -5,5 )
+		var rand_y = randi_range ( -5,5 )
+		enemy.position = get_parent().map_to_local (Vector2i(tile_pos.x-rand_x,tile_pos.y-rand_y))
+		print("Adding at "+str(get_parent().local_to_map(enemy.position)))
+
+func _move_enemies():
+	get_parent().get_tree().call_group("monsters", "_move_randomly")
