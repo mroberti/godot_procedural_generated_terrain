426cbc6b0ac4be42eb09cc052931bc743222214d
Mario Roberti <mario.roberti@gmail.com>
2023-11-15 08:59:50 -0800

    Trying to implement Minifantasy tiles
    Trying to implement Minifantasy tiles

--- TileMap.gd
+++ TileMap.gd
@@ -3,8 +3,8 @@ extends TileMap
 var moisture = FastNoiseLite.new()
 var temperature = FastNoiseLite.new()
 var altitude = FastNoiseLite.new()
-var width = 5
-var height = 5
+var width = 25
+var height = 25
 const city_icon = preload("res://scenes/village.tscn")
 var _timer = null
 @onready var player = $"../Player"
@@ -43,13 +43,38 @@ func generate_chunk(position):
 	for x in range(width):
 		for y in range(height):
 			var coords = Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)
-			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
-			var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
+			print(coords)
 			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*150
-			var atlas_coords = Vector2(3 if alt < 2 else round((moist + 10) / 5), round((temp + 10) / 5))
+			var atlas_coords
+			# 1,9 Grass
+			# 2,33 Water
+			print(alt)
 			if(get_cell_atlas_coords(0,coords,false) == Vector2i(-1,-1)):
 				# A cell doesn't exist at these coordinates, so 
 				# we're creating one
-				set_cell(0, coords,1, atlas_coords, 0 )
+				if(alt>27):
+					atlas_coords = Vector2(1,9)
+				else:
+					atlas_coords = Vector2(2,33)
+				set_cell(0, coords,0, atlas_coords, 0 )
+				#print("Tile doesn't exist yet")
 			else:
 				pass;
+
+# This is the old method
+# func generate_chunk(position):
+# 	var tile_pos = local_to_map(position)
+				
+# 	for x in range(width):
+# 		for y in range(height):
+# 			var coords = Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)
+# 			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
+# 			var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
+# 			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*150
+# 			var atlas_coords = Vector2(3 if alt < 2 else round((moist + 10) / 5), round((temp + 10) / 5))
+# 			if(get_cell_atlas_coords(0,coords,false) == Vector2i(-1,-1)):
+# 				# A cell doesn't exist at these coordinates, so 
+# 				# we're creating one
+# 				set_cell(0, coords,1, atlas_coords, 0 )
+# 			else:
+# 				pass;
